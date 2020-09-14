b955f3d2a97ef0735df2d7b876b178597603f100
Hoang Nguyen <hoang.nguyen2@niteco.se>
2020-09-14 17:52:36 +0700

    init

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import {readFileSync} from 'fs';
+
+const app = express();
+
+app.use('/', (req, res, next) => {
+  next();
+});
+
+app.get('/article', (req, res) => {
+  let listData = readFileSync('list.json', 'utf8');
+  res.send(listData);
+});
+
+app.get('/article/:id', (req, res) => {
+  let data = readFileSync('data.json', 'utf8');
+  res.send(JSON.parse(data)[req.params.id]);
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000');
+});
